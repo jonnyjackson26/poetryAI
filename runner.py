4d4f606e16a741aa20f892c434e82d7743f5fc26
jrsjackson26 <jrsjackson26@gmail.com>
2024-05-15 10:12:15 -0400

    now writes to a file rather than outputting to terminal

--- runner.py
+++ runner.py
@@ -2,11 +2,10 @@ from WordInfo import WordInfo
 
 
 def main():
-    path="1nephi1.txt"
+    path="input/1nephi1.txt"
     try:
         with open(path, 'r') as file:
             file_contents = file.read()
-            #print("File contents:",file_contents)
             #convert to lower case
             file_contents = file_contents.lower()
             #remove characters 
@@ -14,7 +13,6 @@ def main():
             for char in characters_to_remove:
                 file_contents = file_contents.replace(char, '')
             file_contents = file_contents.replace(".", ' ')
-            #print("updated File contents:",file_contents)
 
             table={}
             words=file_contents.split(" ")
@@ -28,8 +26,12 @@ def main():
                     table[current_word] = WordInfo()
                     table[current_word].addFollowingWord(next_word)
 
-            for word in table:
-                print(word,table[word].toString())
+            
+            #write results to ouptut/output.txt
+            with open("output/output.txt", "w") as output_file:
+                for word in table:
+                    output_file.write(word + " " + table[word].toString() + "\n")
+
 
 
 
