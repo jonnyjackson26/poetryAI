d2556a5225526e23211f93cc4f4312a6251fedb1
jrsjackson26 <jrsjackson26@gmail.com>
2024-05-15 11:29:49 -0400

    added .gitignore for input and output

--- runner.py
+++ runner.py
@@ -5,16 +5,17 @@ import re
 def main():
     path="input/1nephi1.txt"
     try:
-        with open(path, 'r') as file:
+        with open(path, "r", encoding="utf-8") as file:
             file_contents = file.read()
             #convert to lower case
             file_contents = file_contents.lower()
             #remove characters 
-            characters_to_remove = "()[],<>@#$%^&*-+=;\n"
+            characters_to_remove = "()[],<>@#$%^&*'\"-+=;"
             for char in characters_to_remove:
                 file_contents = file_contents.replace(char, '')
             # Add a space before and after '.', '!', '?', making them words
             file_contents = re.sub(r'(?<=[^.!?])([.!?])', r' \1 ', file_contents)
+            file_contents = file_contents.replace("\n", ' ')
 
             
             table=Table(file_contents.split(" "))
